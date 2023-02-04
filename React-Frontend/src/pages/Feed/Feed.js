1fce882195a8ae4710d8ec7ef433bcabde0c0139
Gaurav <gauravrajanju@gmail.com>
2023-02-04 18:36:57 +0530

    config create user

--- React-Frontend/src/pages/Feed/Feed.js
+++ React-Frontend/src/pages/Feed/Feed.js
@@ -9,6 +9,8 @@ import Loader from '../../components/Loader/Loader';
 import ErrorHandler from '../../components/ErrorHandler/ErrorHandler';
 import './Feed.css';
 
+const url = "http://localhost:8080/graphql"
+
 
 class Feed extends Component {
   state = {
@@ -136,35 +138,50 @@ class Feed extends Component {
     formData.append('title', postData.title);
     formData.append('content', postData.content);
     formData.append('image', postData.image);
-    let url = 'http://localhost:8080/feed/post';
-    let method = 'POST';
-    if (this.state.editPost) {
-      url = 'http://localhost:8080/feed/post/' + this.state.editPost._id;
-      method = 'PUT'
+
+    let graphqlQuery = {
+      query: `
+      mutation {
+        createPost(postInput : { title : "${postData.title}", content : "${postData.content}", imageUrl : "some url" }) {
+          _id
+          title
+          content
+          imageUrl
+          creator { name }
+          createdAt
+        }
+      }
+      `
     }
 
     fetch(url, {
-      method: method,
-      body: formData,
+      method: 'POST',
+      body: JSON.stringify(graphqlQuery),
       headers: {
-        Authorization: 'Bearer ' + this.props.token
+        Authorization: 'Bearer ' + this.props.token,
+        'Content-Type': 'application/json'
       }
     })
       .then(res => {
         console.log(res);
-        if (res.status !== 200 && res.status !== 201) {
-          throw new Error('Creating or editing a post failed!');
-        }
         return res.json();
       })
       .then(resData => {
+        if (resData.errors && resData.errors.status === 422) {
+          throw new Error(
+            "Validation failed. Make sure the email address isn't used yet!"
+          );
+        }
+        if (resData.errors) {
+          throw new Error('User creation failed');
+        }
         console.log(resData);
         const post = {
-          _id: resData.post._id,
-          title: resData.post.title,
-          content: resData.post.content,
-          creator: resData.post.creator,
-          createdAt: resData.post.createdAt
+          _id: resData.data.createPost._id,
+          title: resData.data.createPost.title,
+          content: resData.data.createPost.content,
+          creator: resData.data.createPost.creator,
+          createdAt: resData.data.createPost.createdAt
         };
         this.setState(prevState => {
           return {
